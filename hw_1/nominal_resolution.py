24c9cb207f13fb5079d0e920ecfee869056bb5a7
Spiridonov <56498495+AntonSpiridonov61@users.noreply.github.com>
2021-11-01 13:01:56 +0800

    Create nominal_resolution.py

---
+++ hw_1/nominal_resolution.py
@@ -0,0 +1,15 @@
+import numpy as np
+from collections import Counter
+
+for i in range(1, 7):
+    with open(f'src/figure{i}.txt') as file:
+        size_mm = float(file.readline())
+        file.readline()
+        image = np.loadtxt(file)
+        index_ones = np.nonzero(image[:])
+        count_elem = Counter(index_ones[0])
+        if count_elem:
+            cnt_pix = max([elem for elem in count_elem.values()])
+            print(f'figure{i} nominal_resolution: {size_mm / cnt_pix}')
+        else:
+            print(f'figure{i} Image not found')
