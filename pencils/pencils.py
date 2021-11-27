8474aa10f0ff076e6f7fc1d77c02aca426e23493
AntonSpiridonov61 <spiridonov61a@gmail.com>
2021-11-27 17:05:06 +0800

    binary

--- pencils/pencils.py
+++ pencils/pencils.py
@@ -1,16 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from skimage.color import rgb2gray
+from skimage import filters
+from skimage.measure import regionprops, label
 
-files = os.listdir()
-print(files)
-for item in os.walk('pencils/source/'):
-    print(item)
-    image = plt.imread(item)
-    plt.imshow(image)
-    plt.show()
+# from skimage.filters import try_all_threshold
 
 
-# image = plt.imread('source/img (1).jpg')
-# plt.imshow(image)
-# plt.show()
+
+
+
+PATH = 'pencils/source/'
+
+for item in os.listdir(PATH):
+    image = plt.imread(PATH + item)[10:-30, 10:-30]
+    gray = rgb2gray(image)
+    thresh = filters.threshold_isodata(gray)
+    binary = gray < thresh
+
+    labeled = label(binary)
+    plt.imshow(labeled)
+    regions = regionprops(labeled)
+    for region in regions:
+        if region.area > 20000:
+            plt.imshow(region.image, cmap='gray')
+            plt.show()
+
+    # fig, ax = try_all_threshold(gray, figsize=(10, 8), verbose=False)
+    # plt.show()
+
+
