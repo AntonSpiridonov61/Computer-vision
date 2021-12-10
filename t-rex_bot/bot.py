f0ea8dc879187217093ebeb15b0939fdd228b9ea
AntonSpiridonov61 <spiridonov61a@gmail.com>
2021-12-10 23:57:25 +0800

    fisrt bird

--- t-rex_bot/bot.py
+++ t-rex_bot/bot.py
@@ -1,12 +1,37 @@
-qimport cv2
+import cv2
 import mss
 import numpy as np
 import pyautogui
-import time
+from time import sleep
 import matplotlib.pyplot as plt
 
-def press_space():
-    pass
+def grab(bbox):
+    img = np.array(sct.grab(bbox))
+    # binary = np.sum(img, 2)
+    # binary[binary > 0] = 1
+    binary = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
+    # cv2.imshow('image', cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
+    cv2.imshow('image', binary)
+
+    # cnt_black_pxl = np.sum(img < 100)
+    # cnt_white_pxl = np.sum(img > 100)
+    # a = sum(map(sum, binary))
+    cactus = binary[40,:]
+    bird = binary[3,:]
+    # cactus = img[45,:,0]
+    # bird = img[5,:,0]
+
+    sum_cactus = np.sum(cactus)
+    sum_bird = np.sum(bird)
+    # print(sum_cactus)
+
+    return sum_cactus, sum_bird
+
+def press_up():
+    pyautogui.keyDown('space')
+    sleep(0.001)
+    pyautogui.keyUp('space')
+    sleep(0.001)
     # pyautogui.press('down')
     # pyautogui.keyUp('down')
     # pyautogui.keyDown('space')
@@ -15,42 +40,36 @@ def press_space():
     # pyautogui.keyDown('down')
 
 with mss.mss() as sct:
-    bbox = {"top": 325, "left": 250, "width": 120, "height": 65}
+    bbox = {"top": 330, "left": 285, "width": 70, "height": 50}
     flag = 1
-    val = 29640
+    val = 17290
     while True:
-        img = np.array(sct.grab(bbox))
-        # cv2.imshow('image', cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
-
-        # cnt_black_pxl = np.sum(img < 100)
-        # cnt_white_pxl = np.sum(img > 100)
-       
-
-        cactus = img[45,:,0]
-        bird = img[3,:,0]
-        print(cactus)
-        print(bird)
-
-        # plt.subplot(121)
-        # plt.imshow(cactus)
-        # plt.subplot(122)
-        # plt.imshow(bird)
-        # plt.show()
-
-        sum_cactus = np.sum(cactus)
-        sum_bird = np.sum(bird)
+        
+        sum_cactus, sum_bird = grab(bbox)
         # print(sum_cactus)
-
         if sum_cactus < val:
-            pyautogui.press('up')
+            # press_up()
+            pyautogui.keyDown('up')      
+            sleep(0.01)             
+            # pyautogui.keyUp('space')
+        # if sum_cactus == val:
+        #     pyautogui.keyUp('up')
+            # pyautogui.press('down')
+
         if sum_bird < val:
-            pyautogui.press('down')
+            # pyautogui.press('down')
             # print(bird)
             print(sum_bird)
             flag = 1
-        if sum_bird and flag == 1:
+            pyautogui.keyDown('down')
+            sleep(0.5)
+            # pyautogui.keyUp('down')
+
+        if sum_bird == val and flag == 1:
             pyautogui.keyUp('down')
             flag = 0
+            print('flag')
+            print(sum_bird)
         
 
         #light mode
