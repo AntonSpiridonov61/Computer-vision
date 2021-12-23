ac30ec1a5869ec830b76da20211edf30cc310664
AntonSpiridonov61 <spiridonov61a@gmail.com>
2021-12-23 14:26:09 +0800

    use major length

--- pencils/pencils.py
+++ pencils/pencils.py
@@ -1,22 +1,31 @@
 import matplotlib.pyplot as plt
 import os
 from skimage.color import rgb2gray
-from skimage import filters
+from skimage import filters, morphology
 from skimage.measure import regionprops, label
 
 PATH = 'pencils/source/'
 cnt_pencils = 0
+major_list = []
+
 
 for item in os.listdir(PATH):
     image = plt.imread(PATH + item)[20:-40, 20:-100]
     
     gray = rgb2gray(image)
     thresh = filters.threshold_isodata(gray)
-    binary = gray.copy() <= thresh
+    binary = gray <= thresh
+    binary = morphology.remove_small_objects(binary)
+    binary = morphology.remove_small_holes(binary)
     labeled = label(binary)
     regions = regionprops(labeled)
-    temp = []
     for region in regions:
-        pass
+        length = region.major_axis_length
+        major_list.append(length)
+
+max_major = max(major_list)
+for i in major_list:
+    if i / max_major > 0.8:
+        cnt_pencils += 1
 
 print(f'All pencils = {cnt_pencils}')
